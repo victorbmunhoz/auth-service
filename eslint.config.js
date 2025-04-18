0fb909b3aad6a1ebd36f6b53236dbfb430fd2983
Victor <vbm2906@gmail.com>
2025-04-18 14:13:29 -0300

    Corrigir configuração do ESLint para CI/CD

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    ignores: ['node_modules/**', 'coverage/**'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        process: 'readonly',
+        module: 'writable',
+        console: 'readonly',
+        __dirname: 'readonly',
+        exports: 'writable',
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      'no-console': 'warn',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    },
+    env: {
+      node: true,
+      es6: true,
+      jest: true
+    }
+  }
+]; 
